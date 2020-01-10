ef9c6bd346e60fe76d76607a35e8b15af65c8d43
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-01-10 06:03:08 +0000

    updated font

--- @types/nostr-relaypool/nostr-relaypool.d.ts
+++ @types/nostr-relaypool/nostr-relaypool.d.ts
@@ -2,4 +2,11 @@
 declare module "merge-similar-filters" {
   import type { Filter } from "node_modules/nostr-tools/index";
   export function mergeSimilarAndRemoveEmptyFilters(
-    fi
+    filters: Filter[]
+  ): Filter[];
+}
+declare module "fakejson" {
+  export function getHex64(json: string, field: string): string;
+  export function getSubName(json: string): string;
+  export function getInt(json: string, field: string): number;
+  export function matchEventI
