0775958b8760b2ab92504e48186e4ea89efe5ca8
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-01-11 19:10:48 +0000

    size_t

--- @types/nostr-relaypool/nostr-relaypool.d.ts
+++ @types/nostr-relaypool/nostr-relaypool.d.ts
@@ -9,4 +9,9 @@ declare module "fakejson" {
   export function getHex64(json: string, field: string): string;
   export function getSubName(json: string): string;
   export function getInt(json: string, field: string): number;
-  export function matchEventI
+  export function matchEventId(json: string, id: string): boolean;
+  export function matchEventPubkey(json: string, pubkey: string): boolean;
+  export function matchEventKind(json: string, kind: number): boolean;
+}
+declare module "relay" {
+  import { type Event } from "node_modul
