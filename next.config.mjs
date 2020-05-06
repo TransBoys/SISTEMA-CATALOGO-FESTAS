8927b23b5057bab5511c1c4194f6df3a6f8fe6c2
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-05-06 09:51:51 +0000

    remove dummy file

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+
+// @ts-check
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+
+/** @type {import("next").NextConfig} */
+const config = {
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ["void.cat"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+
+  /**
+   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+};
+export default config;
