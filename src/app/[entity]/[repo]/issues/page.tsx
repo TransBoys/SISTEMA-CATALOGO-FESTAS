e03f1bc0bcd1faca2c5e5c07eec73980d7b92f73
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-06-12 14:38:18 +0000

    Code review changes.

--- src/app/[entity]/[repo]/issues/page.tsx
+++ src/app/[entity]/[repo]/issues/page.tsx
@@ -3,4 +3,18 @@
 import * as React from "react";
 import { useCallback, useEffect, useState } from "react";
 
-import FilterBar from 
+import FilterBar from "@/components/filter-bar";
+import { Button } from "@/components/ui/button";
+
+import { clsx } from "clsx";
+import {
+  Check,
+  CheckCircle2,
+  ChevronDown,
+  CircleDot,
+  GitPullRequest,
+  MessageSquare,
+  Search,
+  Tag,
+} from "lucide-react";
+import
