3b333c5db8a86690d58430ce88b4f43dc6ac7b32
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-07-15 14:44:14 +0000

    Update JQuery-form-validator.js

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,2 +1,11 @@
 export default function Head() {
- 
+  return (
+    <>
+      <title>NostrGit</title>
+      <meta
+        name="description"
+        content="a truly censorship-resistant alternative to GitHub that has a chance of working"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <link
+       
