53a707d5b4273613854a52f1288e122cce4afd07
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-07-24 10:26:55 +0000

    Renamed.

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,4 +8,16 @@ export default function Head() {
       />
       <link rel="icon" href="/favicon.ico" />
       <link
-       
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png
