bd2fff72de0712c0d0f98cf747a024a2a51db49c
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-07-26 19:06:00 +0000

    I18n: Update translation id (100%).

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,3 +1,14 @@
 import Link from "next/link";
 
-export
+export default function Banner({
+  title,
+  description,
+  link,
+}: {
+  title: string;
+  description: string;
+  link: string;
+}) {
+  return (
+    <>
+      <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:p
