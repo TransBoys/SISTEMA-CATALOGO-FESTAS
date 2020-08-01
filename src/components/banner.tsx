41b638b984607edb7fe94b0bf3661eb68b684af6
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-01 21:54:42 +0000

    Fix test failures

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -11,4 +11,8 @@ export default function Banner({
 }) {
   return (
     <>
-      <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:p
+      <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
+        <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-purple-900 py-2.5 px-6 sm:rounded-xl sm:py-3 sm:pr-3.5 sm:pl-4">
+          <p className="text-sm leading-6 text-white">
+            <Link href={link}>
+           
