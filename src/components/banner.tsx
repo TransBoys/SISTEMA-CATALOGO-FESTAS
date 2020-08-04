a269e4b55394bc1f47de881f5321a65aa7d2e4b8
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-04 05:53:04 +0000

    Update init-3rd-party.el

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -15,4 +15,10 @@ export default function Banner({
         <div className="pointer-events-auto flex items-center justify-between gap-x-6 bg-purple-900 py-2.5 px-6 sm:rounded-xl sm:py-3 sm:pr-3.5 sm:pl-4">
           <p className="text-sm leading-6 text-white">
             <Link href={link}>
-           
+              <strong className="font-semibold">{title}</strong>
+              <svg
+                viewBox="0 0 2 2"
+                className="mx-2 inline h-0.5 w-0.5 fill-current"
+                aria-hidden="true"
+              >
+                <circle cx={1
