312f8baae68768e2cc6c649862d9f9ba8da4f8f1
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-09 21:11:36 +0000

    Removed unused vars

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,2 +1,12 @@
 "use client";
 
+import * as React from "react";
+
+import { MobileNav } from "@/components/mobile-nav";
+import { cn } from "@/lib/utils";
+
+import { Bell, Menu, X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import Logo 
