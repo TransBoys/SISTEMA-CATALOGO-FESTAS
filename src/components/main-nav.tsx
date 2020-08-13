4d86158d6495756cb037d876cd4be686dcc1183a
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-13 08:26:22 +0000

    Refactoring in progress

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -9,4 +9,16 @@ import { Bell, Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-import Logo 
+import Logo from "./logo";
+import SearchBar from "./search-bar";
+import { Input } from "./ui/input";
+
+export type NavItem = {
+  title: string;
+  href: string;
+  disabled?: boolean;
+};
+
+export type MainNavItem = NavItem;
+
+export type HeaderConfig 
