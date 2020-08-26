3d9814ea5aad8fa439393f89a8ad9f02f27e439d
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-26 08:15:02 +0000

    17

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -3,4 +3,10 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import * as Ava
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+
+const Avatar = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <Avata
