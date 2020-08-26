79be21d3eafda83b7892a6acb32697a7a07dbb9b
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-26 14:03:34 +0000

    update snippets

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -9,4 +9,16 @@ const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
 >(({ className, ...props }, ref) => (
-  <Avata
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      className
+    )}
+    {...props}
+  />
+));
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = React.forwardRef<
+  React.
