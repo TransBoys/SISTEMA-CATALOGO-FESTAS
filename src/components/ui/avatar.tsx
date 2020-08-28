ea8240a2ddf46fec2e692e60f9d1a25e40a775e5
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-28 12:58:42 +0000

    Fixing test cases

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -35,4 +35,14 @@ AvatarImage.displayName = AvatarPrimitive.Image.displayName;
 const AvatarFallback = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Fallback>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
->(({ cl
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-zinc-100 dark:bg-zinc-700",
+      className
+    )}
+    {...props}
+  />
+));
+AvatarFallback.
