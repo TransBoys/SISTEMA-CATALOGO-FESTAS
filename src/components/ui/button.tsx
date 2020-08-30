e8edf78b74939bac35e8cbd6537003e49a7f8fbd
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-30 10:56:04 +0000

    Remove stale code

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,4 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-import { type VariantProps, cva } from "c
+import { type VariantProps, cva } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-0 focus:ring-zinc-400 focus:ring-offset-0 dark:hover:bg-zinc-800 dark:hover:text-zinc-100 d
