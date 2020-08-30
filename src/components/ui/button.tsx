450940561af862f41f7f0c33e39c17ad61319fba
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-08-30 17:05:41 +0000

    Removed a completed task.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,4 +5,9 @@ import { cn } from "@/lib/utils";
 import { type VariantProps, cva } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-0 focus:ring-zinc-400 focus:ring-offset-0 dark:hover:bg-zinc-800 dark:hover:text-zinc-100 d
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-0 focus:ring-zinc-400 focus:ring-offset-0 dark:hover:bg-zinc-800 dark:hover:text-zinc-100 disabled:opacity-50 dark:focus:ring-zinc-400 disabled:pointer-events-none dark:focus:ring-offset-zinc-900 data-[state=open]:bg-zinc-100 dark:data-[state=open]:bg-zinc-800",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-zinc-900 text
