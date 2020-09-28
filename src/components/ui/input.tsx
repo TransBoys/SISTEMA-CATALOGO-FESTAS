0973ceced38b46963db6694ef07ec708a728f76e
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-09-28 08:40:23 +0000

    no print

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,4 +2,11 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-export type InputProps = React.InputHTMLAt
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          "flex h-8 w-full rounded-md border border-zinc-300 bg-transparent py-2
