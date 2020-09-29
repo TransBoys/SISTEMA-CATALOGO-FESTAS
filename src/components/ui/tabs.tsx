1158a2e3de58f885cc21ed8cb3005b4cae95b87e
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-09-29 17:21:30 +0000

    Pequenas correções

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -2,4 +2,11 @@
 
 import * as React from "react";
 
-import { cn 
+import { cn } from "@/lib/utils";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+
+const Tabs = TabsPrimitive.Root;
+
+const TabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimiti
