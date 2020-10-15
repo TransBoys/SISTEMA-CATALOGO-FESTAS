5f63c9f6faf50ceb2644c40fc6a196cb9a08f784
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-10-15 16:31:36 +0000

    Links fixed

--- src/env.mjs
+++ src/env.mjs
@@ -1,1 +1,12 @@
-i
+import { z } from "zod";
+
+/**
+ * Specify your server-side environment variables schema here. This way you can ensure the app isn't
+ * built with invalid env vars.
+ */
+const server = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]),
+});
+
+/**
+ 
