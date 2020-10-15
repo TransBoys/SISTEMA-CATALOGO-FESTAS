84ce377c23389de2e603ba9efd7b0ceff3a38647
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-10-15 06:07:54 +0000

    Upgraded gem(s): rubocop

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -8,4 +8,5 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
-  
+        className={cn(
+          "flex h-20 w-full rounded-md border border-zinc-300 bg-transparent py-2 px-3 text-sm placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 da
