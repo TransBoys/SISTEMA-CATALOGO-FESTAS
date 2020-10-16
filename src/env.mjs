56f18d086d922be1e90188153a6f24fba9f570d4
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-10-16 08:37:23 +0000

    temp

--- src/env.mjs
+++ src/env.mjs
@@ -13,4 +13,11 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.st
+  // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+});
+
+/**
+ * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
+ * middlewares) or client-side so we need to destruct manually.
+ *
+ * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client
