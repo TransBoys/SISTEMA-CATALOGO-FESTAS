d465cdfa35919d6fa355b5b993f02b2c1e30444e
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-10-18 18:22:50 +0000

    Fixed UI

--- src/env.mjs
+++ src/env.mjs
@@ -20,4 +20,16 @@ const client = z.object({
  * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
  * middlewares) or client-side so we need to destruct manually.
  *
- * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client
+ * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
+ */
+const processEnv = {
+  NODE_ENV: process.env.NODE_ENV,
+  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+};
+
+// Don't touch the part below
+// --------------------------
+
+const merged = server.merge(client);
+
+/** @typed
