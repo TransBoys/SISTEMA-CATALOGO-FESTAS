791039bb32cc8d7c9c1eb0ecbc3eb49ae58c48a7
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-10-30 21:34:46 +0000

    added more functions

--- src/lib/hooks/useLocalStorage.tsx
+++ src/lib/hooks/useLocalStorage.tsx
@@ -1,3 +1,10 @@
 import { useState } from "react";
 
-export default function useLocalStorage<T>(
+export default function useLocalStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T | null>(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? 
