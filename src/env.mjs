2f1cd5991182a05b2c2b78a88984ea0eed02c29f
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-10-30 07:47:53 +0000

    Added some features with expressions

--- src/env.mjs
+++ src/env.mjs
@@ -59,4 +59,9 @@ if (!!process.env.SKIP_ENV_VALIDATION == false) {
     get(target, prop) {
       if (typeof prop !== "string") return undefined;
       // Throw a descriptive error if a server-side env var is accessed on the client
-      // Otherwise it would just be returning `undefined` a
+      // Otherwise it would just be returning `undefined` and be annoying to debug
+      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
+        throw new Error(
+          process.env.NODE_ENV === "production"
+            ? "❌ Attempted to access a server-side environment variable on the client"
+            : `❌
