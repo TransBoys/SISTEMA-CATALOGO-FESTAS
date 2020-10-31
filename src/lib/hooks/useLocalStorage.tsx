544b7ff89b8b86eee3bbb0db083344aac26eb01e
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-10-31 16:08:22 +0000

    net: mvneta: fix operation in 10 Mbit/s mode

--- src/lib/hooks/useLocalStorage.tsx
+++ src/lib/hooks/useLocalStorage.tsx
@@ -7,4 +7,14 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
     }
     try {
       const item = window.localStorage.getItem(key);
-      return item ? 
+      return item ? (JSON.parse(item) as T) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    if (value === storedValue || value === undefined || value === null) return;
+    try {
+      setStoredV
