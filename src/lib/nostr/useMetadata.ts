04fbffba6309b949b6e27756ea25debb04b04d7d
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-11-17 05:51:28 +0000

    bump cfdot

--- src/lib/nostr/useMetadata.ts
+++ src/lib/nostr/useMetadata.ts
@@ -5,4 +5,15 @@ import { useNostrContext } from "./NostrContext";
 export type Metadata = {
   banner?: string;
   website?: string;
-  nip0
+  nip05?: string;
+  picture?: string;
+  lud16?: string;
+  display_name?: string;
+  about?: string;
+  name?: string;
+};
+
+const useMetadata = (relays: string[] = []) => {
+  const { subscribe, defaultRelays, pubkey } = useNostrContext();
+
+  const [metadata
