b8f3ea734b0a05c0b901fb50de7d7853ed821fb0
TransBoys <113254509+TransBoys@users.noreply.github.com>
2020-11-29 20:58:20 +0000

    HID: input: generic hidinput_input_event handler

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
   darkMode: "class",
-  content: ["./src
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lightgray: "#383B42",
+        dark: "#22262C",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollb
